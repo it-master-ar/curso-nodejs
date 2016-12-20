0ed8d7e4971f1e5c9df7da8ce7d93b229c979add
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-12-20 16:44:42 -0300

    Adding demo samples

--- demo/index.js
+++ demo/index.js
@@ -4,10 +4,12 @@ const http = require('http');
 // crear un web server que responde "Hello Server" a todos los requests.
 const server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Server!');
+  res.end(req.url);
 });
 
 // attachar el servidor al puerto 8000
-server.listen(8000);
+server.listen(process.env.PORT);
+
+console.log(process.argv);
 
 console.log('Server running at :8000');
